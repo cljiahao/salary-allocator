f718342bd12cf34d91fc05d92f5e4be65a632886
cljiahao <cljiahao@hotmail.com>
2025-04-13 23:20:16 +0800

    FEAT: remove goals and add charts and input-form components

---
+++ src/components/calculator/charts.tsx
@@ -0,0 +1,14 @@
+export default function Charts() {
+  return (
+    <div className="">
+      <div>Chart</div>
+      <div>Chart</div>
+      <div>Chart</div>
+      <div>Chart</div>
+      <div>Chart</div>
+      <div>Chart</div>
+      <div>Chart</div>
+      <div>Chart</div>
+    </div>
+  );
+}
---
+++ src/components/calculator/input-form.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+interface InputFormProps {
+  className?: string;
+}
+
+export default function InputForm({ className }: InputFormProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <div className={cn("rounded-xl border-2 p-6", className)}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Monthly Salary</FormLabel>
+                <FormControl>
+                  <Input placeholder="$5,000" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+                <FormLabel>Average Monthly Expenses</FormLabel>
+                <FormControl>
+                  <Input placeholder="$1,000" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+}
