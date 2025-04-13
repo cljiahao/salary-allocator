7fc973cc3de9d8e9a3d871df89a305f16fd2b0fb
cljiahao <cljiahao@hotmail.com>
2025-04-13 23:19:47 +0800

    FEAT: update tailwindcss for layout

--- src/app/calculator/layout.tsx
+++ src/app/calculator/layout.tsx
@@ -1,10 +1,12 @@
-import SideNav from "@/components/sidenav";
+import SideNav from "@/components/calculator/sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <SideNav />
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:px-16 md:py-12">
+        {children}
+      </div>
     </div>
   );
 }
